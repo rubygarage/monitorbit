c8d5e90d91f836c35195ee64a41464f733a27fa0
dyachenko-yaroslav <mrthefoton@gmail.com>
2019-09-02 08:41:15 +0300

    Add necessary Yabeda initializers

--- lib/monitorbit/railtie.rb
+++ lib/monitorbit/railtie.rb
@@ -6,6 +6,18 @@ require 'prometheus/client'
 
 module Monitorbit
   class Railtie < Rails::Railtie
+    initializer 'monitorbit.use_yabeda_prometheus_exporter_middleware' do |app|
+      app.middleware.use Yabeda::Prometheus::Exporter
+    end
+
+    initializer 'monitorbit.set_up_sidekiq' do |app|
+      Sidekiq.configure_server { |config| Yabeda::Prometheus::Exporter.start_metrics_server! }
+    end
+
+    initializer 'monitorbit.install_yabeda_rails' do |app|
+      Yabeda::Rails.install! unless ::Rails.const_defined?(:Server) || ::Rails.const_defined?('Puma::CLI')
+    end
+
     generators do
       require_relative '../generators/monitorbit/install'
     end
