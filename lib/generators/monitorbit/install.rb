cbb9fc167fc0d8762863cc2a7bd3cd15c1114c87
dyachenko-yaroslav <mrthefoton@gmail.com>
2019-09-11 20:46:19 +0300

    Update generator

--- lib/generators/monitorbit/install.rb
+++ lib/generators/monitorbit/install.rb
@@ -3,5 +3,9 @@ module Monitorbit
     def create_initializer_file
       append_to_file 'config/puma.rb', "activate_control_app\nplugin :yabeda"
     end
+
+    def add_middleware
+      gsub_file 'config.ru', 'run Rails.application', "use Monitorbit::RequestErrorsLayer\n\nrun Rails.application"
+    end
   end
 end
