58e6bffb6837f0627f819fd5e4a71d3f3dbec207
dyachenko-yaroslav <mrthefoton@gmail.com>
2019-09-20 16:45:31 +0300

    Control missing files for CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
   config.shared_context_metadata_behavior = :apply_to_host_groups
