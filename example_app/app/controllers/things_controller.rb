efdc7b59b03ec6f622d2f25edeac951e2d768db4
dyachenko-yaroslav <38441787+dyachenko-yaroslav@users.noreply.github.com>
2019-10-07 08:35:27 +0000

    Collect information about slowest queries (#9)
    * Add sql metrics exporting logic
    
    * Update version
    
    * Update Gemfile.lock in the example application
    
    * Configure SQL queries panel, add logic to example app
    
    * Remove unnecessary column from Slowest queries table
    
    * Final changes to dashboards
    
    * Update Gemfile.lock
    
    * Update Readme

--- example_app/app/controllers/things_controller.rb
+++ example_app/app/controllers/things_controller.rb
@@ -1,3 +1,20 @@
 class ThingsController < ApplicationController
-  def index; end
+  def index
+    render json: Thing.first(50)
+  end
+
+  def show
+    id = Thing.ids.sample
+    render json: Thing.find(id)
+  end
+
+  def create
+    render json: Thing.create(thing_params)
+  end
+
+  private
+
+  def thing_params
+    params.permit(:name)
+  end
 end
